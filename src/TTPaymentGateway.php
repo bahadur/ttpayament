8b8ddec8dd1bb86803324d753ee192471055647a
bahadur <bahadur.oad@gmail.com>
2024-09-30 01:07:29 +0500

    Setup composer

---
+++ src/TTPaymentGateway.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace TaskuTark\TTPayment;
+
+use GuzzeleHttp\Client;
+
+class TTPaymentGateway
+{
+
+    protected $client;
+    protected $username;
+    protected $paswword;
+
+    public function __construct(string $username, string $paswword)
+    {
+        $this->username = $username;
+        $this->paswword = $paswword;
+        $this->client = new Client();
+
+    }
+
+    public function makePayment(array $paymentDetails): string
+    {
+        $response = $this->client->request('POST', $this->apiIrl, [
+            'headers' => [
+                'Authentication' => 'Basic ',
+                'Content-Type' => 'application/json'
+            ],
+            'json' => $paymentDetails
+        ]);
+
+        return $response->getBody()->getContents();
+    }
+
+
+}
